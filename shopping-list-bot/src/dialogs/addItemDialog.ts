881b18daa99c03f9344ede5b7668d73e1bccb17e
Sandro Speth <sandro.speth@iste.uni-stuttgart.de>
2021-02-02 23:13:26 +0100

    Add item and unit models, and dialogs for add intent
    Remove sample bookingDetails model, add item and unit models, add addItemDialog and unitDialog for the addItem intent

---
+++ shopping-list-bot/src/dialogs/addItemDialog.ts
@@ -0,0 +1,58 @@
+import { InputHints, MessageFactory } from "botbuilder";
+import { ConfirmPrompt, DialogTurnResult, TextPrompt, WaterfallDialog, WaterfallStepContext } from "botbuilder-dialogs";
+import { Item } from "../models/item";
+import { Unit } from "../models/unit";
+import { CancelAndHelpDialog } from "./cancelAndHelpDialog";
+import { UnitDialog } from "./unitDialog";
+
+const TEXT_PROMPT = 'addItemTextPrompt';
+const CONFIRM_PROMPT = 'addItemConfirmPrompt';
+const WATERFALL_DIALOG = 'addItemWaterfallDialog';
+const UNIT_DIALOG = 'addItemUnitDialog';
+
+export class AddItemDialog extends CancelAndHelpDialog {
+    constructor(id: string) {
+        super(id || 'addItemDialog');
+
+        this.addDialog(new TextPrompt(TEXT_PROMPT))
+            .addDialog(new ConfirmPrompt(CONFIRM_PROMPT))
+            .addDialog(new UnitDialog(UNIT_DIALOG))
+            .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
+                this.entityStep.bind(this),
+                this.queryUnitStep.bind(this),
+                this.finalStep.bind(this)
+            ]));
+
+        this.initialDialogId = WATERFALL_DIALOG;
+    }
+
+    private async entityStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
+        const item = stepContext.options as Item;
+
+        if (!item.entity) {
+            const messageText = 'Which item would you like to add?';
+            const message = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.prompt(TEXT_PROMPT, { prompt: message });
+        } else {
+            return await stepContext.next(item.entity);
+        }
+    }
+
+    private async queryUnitStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
+        const entity = stepContext.result as string;
+        const item = stepContext.options as Item;
+        item.entity = entity;
+
+        if (!item.unit) {
+            return await stepContext.beginDialog(UNIT_DIALOG);
+        } else {
+            return await stepContext.next((item.unit));
+        }
+    }
+
+    private async finalStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
+        const unit = stepContext.result as Unit;
+        (stepContext.options as Item).unit = unit;
+        return await stepContext.endDialog(stepContext.options);
+    }
+}
---
+++ shopping-list-bot/src/models/item.ts
@@ -0,0 +1,7 @@
+import { Unit } from "./unit";
+
+export class Item {
+    public intent: string;
+    public unit?: Unit;
+    public entity: string;
+}
