c4ffc46cf11dff6edd35bca62fbe9fdbc08d5244
Malte Reimann <malte.reimann@student.kit.edu>
2021-02-12 22:17:19 +0100

    Start work on update item function
    Removes all legacy function code from migrating to mongo instead of sql

--- functions/UpdateItemFunction/index.ts
+++ functions/UpdateItemFunction/index.ts
@@ -1,16 +1,48 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { Item } from "../models/item";
+import { CosmosDBService } from "../services/cosmosDBService";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const conversationID = context.bindingData.conversationID;
+    if (!conversationID || !req.body || (!req.body.itemName && !req.body.positionInShoppingList)) {
+        context.res = {
+            status: 400,
+            body: {
+                item: req.body,
+                message: 'invalid input'
+            }
+        };
+        context.done();
+    }
+    const body = req.body;
+    let itemToUpdate: Partial<Item>;
+    if(body.positionInShoppingList && body.marked){
+        itemToUpdate = {
+            positionInShoppingList: body.positionInShoppingList,
+            marked: body.marked
+        }
+    }
+    console.dir(itemToUpdate);
+    try {
+        const cosmosService = new CosmosDBService(conversationID);
+       
+        const updatedItem = await cosmosService.updateItem(itemToUpdate);
+        context.res = {
+            status: 200,
+            body: {
+                item: updatedItem
+            }
+        };
+    } catch (error) {
+        context.res = {
+            status: 404,
+            body:  { item: itemToUpdate, message: error }
+        };
+    } finally {
+        context.done();
+    }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+
 
 };
 
--- functions/services/cosmosDBService.ts
+++ functions/services/cosmosDBService.ts
@@ -1,72 +1,43 @@
 import { Container, CosmosClient } from "@azure/cosmos";
 import { Item } from "../models/item";
+import {FilterQuery, MongoClient} from 'mongodb';
 
 export class CosmosDBService {
 
     private readonly conversationID: string;
-    private readonly container: Container;
+    private readonly client: MongoClient;
     constructor(conversationID: string) {
         this.conversationID = conversationID;
-        const client = new CosmosClient(process.env.SHOPPING_LIST_COSMOSDB);
-        this.container = client.database('shopping-list-db').container('ShoppingListContainer');
-
-    }
-
-    public async removeItemByPosition(positionInShoppingList: number): Promise<Item> {
-        const itemID: string = (await this.container.items.query(`SELECT c.id FROM c WHERE c.conversationID = '${this.conversationID}' AND c.item.positionInShoppingList = ${positionInShoppingList}`).fetchNext()).resources[0]?.id;
-        console.log(itemID);
-        if (itemID) {
-            const removedListItem: Item = await this.removeItem(itemID);
-            await this.updateItemPositionsAfterRemoval(positionInShoppingList);
-            return removedListItem;
-        } else {
-            throw new Error('Item could not be removed');
-        }
+        this.client = new MongoClient(process.env.SHOPPING_LIST_COSMOSDB);
     }
 
 
-    public async removeAllItems() {
-        const itemIDs = (await this.container.items.query(`SELECT c.id FROM c WHERE c.conversationID = '${this.conversationID}'`).fetchAll()).resources;
-        const removedItems: Item[] = [];
-        if (itemIDs && itemIDs.length > 0) {
-            for (const id of itemIDs) {
-                removedItems.push(await this.removeItem(id.id));
-            }
-            return removedItems;
-        } else {
-            throw new Error('There are no items for this conversation id');
-        }
-    }
 
-    private async removeItem(itemID: string) {
-        const containerItem = this.container.item(itemID, this.conversationID);
-        const removedListItem: Item = (await containerItem.read()).resource;
-        await containerItem.delete();
-        return removedListItem;
-    }
-
-    private async updateItemPositionsAfterRemoval(positionOfRemoveItem: number) {
-        const itemsToBeUpdated = (await this.container.items.query(`SELECT * FROM c WHERE c.conversationID = '${this.conversationID}' AND c.item.positionInShoppingList > ${positionOfRemoveItem}`).fetchAll()).resources;
-        console.dir(itemsToBeUpdated[0]);
-        const containerItem = this.container.item(itemsToBeUpdated[0], this.conversationID);
+    public async updateItem(itemToUpdate: Partial<Item>) {
         try {
-            const i = new Item('Hello', true, 10);
-            await containerItem.replace(i);
-        } catch (error) {
-            console.log(error);
+        await this.client.connect();
+        const database = this.client.db('shopping-list-db');
+        const collection = database.collection('ItemsContainer');
+        let filter: FilterQuery<any>;
+        if(itemToUpdate.positionInShoppingList && itemToUpdate.itemName === undefined) {
+            filter = {'item.positionInShoppingList': itemToUpdate.positionInShoppingList, 'conversationID': this.conversationID};
+        } else if(itemToUpdate !== undefined) {
+            filter = {'item.itemName': itemToUpdate.itemName, 'conversationID': this.conversationID};
+
+        } else {
+            throw new Error('Need itemNam or positionInShoppingList to update item');
         }
-        console.log("containerItem.id");
-        // containerItem.id
-        // containerItem.replace(container);
-        // console.dir(itemsToBeUpdated);
-        // for (const item of itemsToBeUpdated) {
-        //     const containerItem = this.container.item(item.id, this.conversationID);
-        //     // console.dir(containerItem);
-        //     item.item.positionInShoppingList = item.item.positionInShoppingList++;
-        //     console.dir(item);
-        //     // const updated = await containerItem.replace({ body: item.id });
-        //     // console.dir(updated);
-        // }
-        // console.dir(itemsToBeUpdated);
+        collection.updateOne(filter, {
+            conversationID: this.conversationID,
+            item: itemToUpdate
+        });
+    } catch(err) {
+        console.dir(err);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await this.client.close();
+      }
     }
+
+ 
 }
