2e7a7f0b24b269cf62f69d7d11a201e8234935e2
Malte Reimann <maltereimann@hotmail.de>
2021-03-05 19:04:00 +0100

    Code documentation for marked dialog

--- shopping-list-bot/src/dialogs/markedDialog.ts
+++ shopping-list-bot/src/dialogs/markedDialog.ts
@@ -3,37 +3,69 @@ import { ConfirmPrompt, DialogTurnResult, WaterfallDialog, WaterfallStepContext 
 import { Item, itemAsTextMessage } from "../models/item";
 import { CancelAndHelpDialog } from "./cancelAndHelpDialog";
 
+/**
+ * Define id's for the Dialogs the MarkedDialog is using as sub-dialogs.
+ */
 const CONFIRM_PROMPT = 'markedConfirmPrompt';
 const WATERFALL_DIALOG = 'markedWaterfallDialog';
 
-
+/**
+ * Asks the chat whether or not the bot should mark an item as completed ot not.
+ */
 export class MarkedDialog extends CancelAndHelpDialog {
-    constructor(id: string) {
-        super(id || 'markedDialog');
+    /**
+     * 
+     *@param {string} [id=markedDialog] unique id in the dialog set this dialog is added to to reference this instance of {@link MarkedDialog}.
+     */
+    constructor(id: string = 'markedDialog') {
+        super(id);
 
+        // Add all the dialogs we want to reference/start from within this dialog.
         this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT))
             .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
+                    // Define the steps we need to ask the chat which item to mark or not.
                     this.queryMarkedCompletedStatus.bind(this),
                     this.finalStep.bind(this)
             ]));
 
+        // We start the waterfall dialog so it goes through the steps we defined
         this.initialDialogId = WATERFALL_DIALOG;
-
     }
 
+    /**
+     * Prompt the chat if a certain intem passed as input should be marked.
+     * 
+     * Postcondition: Passes true to the next step if the chat wants to mark the item as completed and false if not. Passes undefined to the next step if something went wrong.
+     * @param stepContext stepContext current context/state of the conversation.
+     * @param stepContext.options the {@link Item} we want to prompt the chat to tell the bot if the item should be marked as completed.
+     * @returns the result of this turn of the dialog. This should not bother us as developers to much since it is handled by the bot framework.
+     */
     private async queryMarkedCompletedStatus(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
         const item = stepContext.options as Item;
-        let messageText = 'Should I mark item as complete?';
         if (item) {
-            messageText = `Should I mark ${itemAsTextMessage(item)} as complete?`;
+            const messageText = `Should I mark ${itemAsTextMessage(item)} as complete?`;
+            const message = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.prompt(CONFIRM_PROMPT, { prompt: message });
+        } else {
+            const somethingWentWrongMessage = "Ugh. I could not understand which item you want to mark. Could you please repeat?";
+            await stepContext.context.sendActivity(somethingWentWrongMessage, somethingWentWrongMessage, InputHints.IgnoringInput);
+            return await stepContext.next(undefined);
         }
-        const message = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
-        return await stepContext.prompt(CONFIRM_PROMPT, { prompt: message });
+        
 
     }
 
+    /**
+     * Handle receiving the result of the previous step, end this dialog and return the result to the parent dialog.
+     * @param stepContext current context/state of the conversation.
+     * @param stepContext.result should be boolean indicating whether or not the user wants to mark the certain item as completed. Can be undefined.
+     * @returns the result of this turn of the dialog. This should not bother us as developers to much since it is handled by the bot framework.
+     */
     private async finalStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
-        const markedValue = stepContext.result as boolean;
-        return await stepContext.endDialog(markedValue);
+        const markedValue = stepContext.result;
+        if(markedValue === undefined) {
+            return await stepContext.endDialog(undefined);
+        }
+        return await stepContext.endDialog(markedValue as boolean);
     }
 }
