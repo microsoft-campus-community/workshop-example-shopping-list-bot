40188753df4901f2b8ca049ee87f3d35413c1590
Malte Reimann <maltereimann@hotmail.de>
2021-03-07 16:35:59 +0100

    JSDoc for Function to remove all items from a shopping list

--- functions/RemoveAllItemsFunction/index.ts
+++ functions/RemoveAllItemsFunction/index.ts
@@ -1,9 +1,24 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { CosmosDBService } from "../services/cosmosDBService";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+/**
+ * Removes/deletes all items from a given shopping list.
+ * 
+ * Precondition: conversationID must not be null, undefined or an empty string.
+ * Postcondition: On success all items from teh shopping list are removed and can not be reverted.
+ *                  On failure no items are removed from the shopping list and an error code and message are returned.
+ * @param context passed from the Azure Functions runtime.
+ * @param context.bindingData.conversationID determines the shopping list from which all items are removed.
+ *         Must not be null, undefined or an empty string. Should be passed through the route parameter of the http request. 
+ * @returns status ```200``` if all items could be removed from the shopping list belonging to the given conversationID.
+ *      Or http status ```404``` if the shopping list could not be found.
+ *      Or http status ```400``` on invalif input.
+ * 
+ * [DELETE] http://<FUNCTION_URL>1/api/RemoveAllItemsFunction/{conversationID}
+ */
+const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
     const conversationID = context.bindingData.conversationID;
-    if (!conversationID) {
+    if (!conversationID || conversationID === '') {
         context.res = {
             status: 400,
             body: {
@@ -21,7 +36,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             body: { message: 'Deleted' }
         };
     } catch (error) {
-        // TODO logger for error
         context.res = {
             status: 404,
             body: { message: 'Not found' }
