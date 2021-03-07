e4ff5d53dbcdb98f7369cfe54ce01d94077d76da
Malte Reimann <maltereimann@hotmail.de>
2021-03-07 16:09:28 +0100

    Document models in the Functions project

--- functions/models/itemDb.ts
+++ functions/models/itemDb.ts
@@ -1,9 +1,36 @@
 import { Unit } from "./unit";
 
+/**
+ * An item in the shopping list.
+ * 
+ * Use for write operations on the shopping list database.
+ * 
+ * Specific for database where id property is set automatically.
+ * As such we do not want to allow modification of id's which is why this class does not contain an id property.
+ * 
+ * {@link Item} represents an item as it is stored in the database.
+ */
 export class ItemDb {
-  public unit?: Unit;
+    /**
+     * Measurement of how much this item is in the shopping list.
+     */
+    public unit?: Unit;
+    /**
+     * Name of the item itself.
+     */
     public itemName: string;
+    /**
+     * True means this item is completed.
+     * False means that is item is not yet completed.
+     */
     public marked: boolean = false;
+    /**
+     * Numerical order within a shopping list. First item in a shopping list has position 1.
+     * Last item in a shopping list has position number of items in the shopping list.
+     * 
+     * Must be unique for each item in the same shopping list.
+     *
+     */
     public positionInShoppingList: number;
 
     constructor(itemName: string, marked: boolean, positionInShoppingList: number, unit?: Unit) {
--- functions/models/unit.ts
+++ functions/models/unit.ts
@@ -1,7 +1,21 @@
+/**
+ * Unit in this domain is a measurement that specifies the amount of an {@link Item}.
+ */
 export class Unit {
+    /**
+     * What unit this object represents (i.e. kg, g, pound, ...).
+     */
     unitName?: string;
+    /**
+     * The amount this {@link Unit} stores.
+     */
     value: number;
 
+    /**
+     * Create a new {@link Unit} that stores {@link value} of {@link unitName} (i.e. 100 kg, ...).
+     * @param unitName describing what {@link Unit} this is.
+     * @param value for the new {@link Unit}.
+     */
     constructor(unitName: string, value: number) {
         if (!value || value <= 0) {
             throw new Error('unit params are not valid');
