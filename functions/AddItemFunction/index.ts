6c149adf783b40d79f9b8d9f74c47076873eaca1
Malte Reimann <maltereimann@hotmail.de>
2021-03-07 16:23:31 +0100

    Document add item Function

--- functions/AddItemFunction/index.ts
+++ functions/AddItemFunction/index.ts
@@ -2,9 +2,25 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { Item } from "../models/item";
 import { CosmosDBService } from "../services/cosmosDBService";
 
+/**
+ * Http endpoint to add an {@link Item} passed in the request to the shopping list belonging to a certain conversationID.
+ * 
+ * Precondition: Item and item's name must not be undefined and item's name must not be an empty string. ConversationID must not be undefined or an empty string.
+ * Postcondition: The given item is added to the shopping list with last position in shopping list and the added item is returned. An error message is returned if adding the item failed.
+ * 
+ * @param context passed from the Azure Functions runtime.
+ * @param req is the http request for this endpoint which should contain an {@link Item} in the body.
+ * @param req.body the {@link Item} to be added to the shopping list.
+ * @param req.body.itemName name for the item to add. Must not be undefined or an empty string.
+ * @param context.bindingData.conversationID item is added to the shopping list belonging to this conversation ID.
+ *         Must not be null, undefined or an empty string. Should be passed through the route parameter of the http request.
+ * @returns the added item in the response body and status 201. Or status 400 and an error mesage in body.message.
+ * 
+ * [POST] http://<FUNCTION_URL>/api/AddItemFunction/{conversationID}
+ */
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     const conversationID = context.bindingData.conversationID;
-    if (!conversationID || !req.body || !req.body.itemName) {
+    if (!conversationID || conversationID === '' || !req.body || !req.body.itemName) {
         console.dir("[DEBUG] ITEM: " + req.body);
         console.dir("[DEBUG] ID: " + conversationID);
         context.res = {
